5354e9063d49068d33a34334c73e41547ab5589a
Gerlinde Christine <gerlindeciocea@yahoo.com>
2020-03-27 23:42:31 +0200

    [ function ] navigate from login to register

---
+++ Application/app/src/main/java/com/example/quizzicat/LoginActivity.kt
@@ -0,0 +1,20 @@
+package com.example.quizzicat
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        redirect_register_link.setOnClickListener {
+            // launch the register activity => do not forget to declare it in the manifest file
+            val registerIntent = Intent(this, RegisterActivity::class.java)
+            startActivity(registerIntent)
+        }
+    }
+}
---
+++ Application/app/src/main/java/com/example/quizzicat/RegisterActivity.kt
@@ -0,0 +1,14 @@
+package com.example.quizzicat
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
