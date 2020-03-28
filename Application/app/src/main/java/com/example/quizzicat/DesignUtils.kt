4c840b324762959feaff36207433e2375e66fa45
Gerlinde Christine <gerlindeciocea@yahoo.com>
2020-03-28 23:42:23 +0200

    [ design ] added custom material snackbar

---
+++ Application/app/src/main/java/com/example/quizzicat/DesignUtils.kt
@@ -0,0 +1,22 @@
+package com.example.quizzicat
+
+import android.content.Context
+import android.util.DisplayMetrics
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
+
+public class DesignUtils {
+    // customized snackbar
+    companion object { // static method in Kotlin
+        fun showSnackbar(view: View, text: String, context: Context) {
+            val snackbar = Snackbar.make(view, text, Snackbar.LENGTH_SHORT)
+            snackbar.view.setBackgroundColor(ContextCompat.getColor(context, R.color.colorAccent))
+            val layoutParams = ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.WRAP_CONTENT)
+            layoutParams.setMargins(0, 1280, 0, 0)
+            snackbar.view.layoutParams = layoutParams
+            snackbar.show()
+        }
+    }
+}
--- Application/app/src/main/java/com/example/quizzicat/RegisterActivity.kt
+++ Application/app/src/main/java/com/example/quizzicat/RegisterActivity.kt
@@ -2,13 +2,24 @@ package com.example.quizzicat
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        register_button.setOnClickListener {
+            val password = register_password.text.toString()
+            val repeatedPassword = register_r_password.text.toString()
+
+            if (password != repeatedPassword) {
+                DesignUtils.showSnackbar(window.decorView.rootView, "Passwords must match!", this)
+            }
+        }
     }
 }
