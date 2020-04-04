cc0a36b588a6895445347c6a2deaadae81de0491
Gerlinde Christine <gerlindeciocea@yahoo.com>
2020-04-04 21:43:48 +0300

    [ refactor ] changed activity_main.xml to activity_login.xml bc it makes more sense

--- Application/app/src/main/java/com/example/quizzicat/LoginActivity.kt
+++ Application/app/src/main/java/com/example/quizzicat/LoginActivity.kt
@@ -3,13 +3,13 @@ package com.example.quizzicat
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
 
         redirect_register_link.setOnClickListener {
             // launch the register activity => do not forget to declare it in the manifest file
