aa9aa7414ade44eb9324bdf454ed9692ecda346a
Gerlinde Christine <gerlindeciocea@yahoo.com>
2020-04-08 02:50:25 +0300

    [ function ] user can create an account with facebook; getting display name and profile picture not implemented yet

--- Application/app/src/main/java/com/example/quizzicat/LoginActivity.kt
+++ Application/app/src/main/java/com/example/quizzicat/LoginActivity.kt
@@ -9,22 +9,36 @@ import android.view.View
 import androidx.appcompat.app.AlertDialog
 import com.example.quizzicat.Exceptions.AbstractException
 import com.example.quizzicat.Exceptions.EmptyFieldsException
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginResult
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.transaction_password_reset.view.*
 
 class LoginActivity : AppCompatActivity() {
 
     private var mFirebaseAuth: FirebaseAuth? = null
+    private var mFirebaseStorage: FirebaseStorage? = null
+    private var mFirebaseDatabase: FirebaseDatabase? = null
 
     private var password: String? = null
     private var email: String? = null
+
+    private lateinit var mCallbackManager: CallbackManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         mFirebaseAuth = FirebaseAuth.getInstance()
+        mFirebaseStorage = FirebaseStorage.getInstance()
+        mFirebaseDatabase = FirebaseDatabase.getInstance()
 
         checkUserSession()
 
@@ -75,13 +89,65 @@ class LoginActivity : AppCompatActivity() {
                 .create()
                 .show()
         }
+
+        login_facebook_icon.setOnClickListener {
+            // forces the onClick handler of the button to execute, even though the button itself is hidden
+            login_facebook_button.performClick()
+        }
+
+        initializeFacebookLogin()
+    }
+
+    private fun initializeFacebookLogin() {
+        // Initialize Facebook Login button
+        mCallbackManager = CallbackManager.Factory.create()
+
+        login_facebook_button.setPermissions("email", "public_profile")
+        login_facebook_button.registerCallback(mCallbackManager, object :
+            FacebookCallback<LoginResult> {
+            override fun onSuccess(loginResult: LoginResult) {
+                handleFacebookAccessToken(loginResult.accessToken)
+            }
+
+            override fun onCancel() {
+
+            }
+
+            override fun onError(error: FacebookException) {
+
+            }
+        })
+    }
+
+    private fun handleFacebookAccessToken(token: AccessToken) {
+        val credential = FacebookAuthProvider.getCredential(token.token)
+        mFirebaseAuth!!.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    val user = mFirebaseAuth!!.currentUser
+                    checkUserSession()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    DesignUtils.showSnackbar(window.decorView.rootView, task.exception!!.message.toString(), this)
+                }
+
+            }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        // Pass the activity result back to the Facebook SDK
+        mCallbackManager.onActivityResult(requestCode, resultCode, data)
+        super.onActivityResult(requestCode, resultCode, data)
     }
 
     private fun checkUserSession() {
         val currentUser = mFirebaseAuth!!.currentUser
-        if (currentUser != null && currentUser.isEmailVerified) {
-            val mainMenuIntent = Intent(this, MainMenuActivity::class.java)
-            startActivity(mainMenuIntent)
+        if (currentUser != null) {
+            if ( ( currentUser.providerData[1].providerId == "password" && currentUser.isEmailVerified ) || currentUser.providerData[1].providerId != "password") {
+                val mainMenuIntent = Intent(this, MainMenuActivity::class.java)
+                startActivity(mainMenuIntent)
+            }
         }
     }
 
