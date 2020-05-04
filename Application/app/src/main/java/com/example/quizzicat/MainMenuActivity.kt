c7089634a2fd1fe3ed6d7037de9e5a14e22031f6
Gerlinde Christine <gerlindeciocea@yahoo.com>
2020-05-05 00:06:51 +0300

    [ design ] tabs + navigation

--- Application/app/src/main/java/com/example/quizzicat/MainMenuActivity.kt
+++ Application/app/src/main/java/com/example/quizzicat/MainMenuActivity.kt
@@ -1,16 +1,37 @@
 package com.example.quizzicat
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.facebook.login.LoginManager
-import com.google.firebase.auth.FirebaseAuth
-import kotlinx.android.synthetic.main.activity_main_menu.*
+import androidx.viewpager2.widget.ViewPager2
+import com.example.quizzicat.Adapters.MainMenuViewPagerAdapter
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainMenuActivity : AppCompatActivity() {
+
+    private val mTabsTitles = ArrayList<Int>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
+
+        setTitlesForTabs()
+
+        val menuViewPager = findViewById<ViewPager2>(R.id.main_menu_viewpager)
+        menuViewPager.adapter = MainMenuViewPagerAdapter(this)
+
+        val menuTabLayout = findViewById<TabLayout>(R.id.main_menu_tabs_layout)
+        TabLayoutMediator(menuTabLayout, menuViewPager) { tab, position ->
+            tab.icon = getDrawable(mTabsTitles[position])
+            menuViewPager.setCurrentItem(tab.position, true)
+        }.attach()
+    }
+
+    fun setTitlesForTabs() {
+        mTabsTitles.add(R.drawable.single_player_tab)
+        mTabsTitles.add(R.drawable.multiplayer_tab)
+        mTabsTitles.add(R.drawable.questions_tab)
+        mTabsTitles.add(R.drawable.profile_tab)
+        mTabsTitles.add(R.drawable.settings_tab)
     }
 }
